1f257612dbefb93156b4b9a79903082c76fb6cd9
Douglas Poma Escritório <douglaspoma@yahoo.com>
2019-11-01 17:32:43 -0300

    dasij

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // require('dotenv/config');
 // const { ApolloServer } = require('apollo-server');
 const express = require('express');
-/* const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 const { importSchema } = require('graphql-import');
 const resolvers = require('./resolvers');
 const context = require('./config/context');
 
-const schemaPath = './schema/index.graphql'; */
+const schemaPath = './schema/index.graphql';
 const app = express();
 
 const port = process.env.PORT || 4000;
 
-/* const server = new ApolloServer({
+const server = new ApolloServer({
   typeDefs: importSchema(schemaPath),
   resolvers,
   context,
@@ -20,13 +20,13 @@ const port = process.env.PORT || 4000;
   cors: {
     origin: '*',
   },
-}); */
+});
 
 app.get('/', (req, res) => {
   res.send('Hello Nodejs!' + process.env.PORT);
 });
 
-/* server.applyMiddleware({ app }); */
+server.applyMiddleware({ app });
 
 app.listen(port, err => {
   if (!err) console.log('Apollo server on');
