0c5fbad886fad315b4cd6e17aef29885194bf1f7
Douglas Poma <pia_da_lan@yahoo.com.br>
2019-11-06 01:56:41 -0300

    Rodar em localserver apenas

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.listen(port, err => {
+app.listen(port, '127.0.0.1', err => {
   if (!err) console.log('Apollo server on');
   else console.log(err);
 });
