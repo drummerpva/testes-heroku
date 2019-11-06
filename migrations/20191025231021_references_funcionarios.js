5744837fe502a24deb3a9392fad37fab2945991d
Douglas Poma <pia_da_lan@yahoo.com.br>
2019-11-06 00:51:30 -0300

    Ajuste Migrations

--- migrations/20191025231021_references_funcionarios.js
+++ migrations/20191025231021_references_funcionarios.js
@@ -1,20 +1,34 @@
 exports.up = function(knex) {
-  return knex.schema.alterTable('funcionarios', table => {
-    table
-      .integer('setor_id')
-      .unsigned()
-      .references('setores.id')
-      .onDelete('NO ACTION')
-      .onUpdate('NO ACTION');
-    table
-      .integer('tipo_pessoa_id')
-      .unsigned()
-      .notNullable()
-      .references('tipos_pessoas.id')
-      .onDelete('NO ACTION')
-      .onUpdate('NO ACTION')
-      .defaultTo(3);
-  });
+  return knex.schema
+    .alterTable('funcionarios', table => {
+      table
+        .integer('setor_id')
+        .unsigned()
+        .references('setores.id')
+        .onDelete('NO ACTION')
+        .onUpdate('NO ACTION');
+      table
+        .integer('tipo_pessoa_id')
+        .unsigned()
+        .notNullable()
+        .references('tipos_pessoas.id')
+        .onDelete('NO ACTION')
+        .onUpdate('NO ACTION')
+        .defaultTo(3);
+    })
+    .then(() => {
+      return knex('funcionarios').insert([
+        {
+          nome: 'ADMINISTRADOR',
+          fantasia: 'ADMIN',
+          cpf_cnpj: '00000000000',
+          cidade: 'ADMIN',
+          uf: 'SC',
+          setor_id: 1,
+          tipo_pessoa_id: 5,
+        },
+      ]);
+    });
 };
 
 exports.down = function(knex) {
